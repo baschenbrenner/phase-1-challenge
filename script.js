2bd4a506a6cc7e186f7e2d5d8ab6cd613faddcb3
martha ochoa <marthaeochoa98@gmail.com>
2022-12-01 07:52:25 -0600

    modified card in css, added classname in js

--- script.js
+++ script.js
@@ -1,4 +1,94 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("LOADED")
 
-document.querySelector("body").appendChild(h2);
+    getRecipients()
+
+    const form = document.querySelector("#recipient-form")
+    form.addEventListener('submit', addRecipient)
+
+    const totalBudget = document.getElementById("total")
+    totalBudget.addEventListener('mouseover', function(){
+      totalBudget.innerText = "Here I am!"
+    })
+    totalBudget.addEventListener('mouseout', function(){
+    totalBudget.innerText = "Total Budget"
+    })
+})
+
+function renderOneRecipient(recipient){
+   
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+    <div id="content">
+    <h2>${recipient.name}</h2>
+    <p>Gift idea: ${recipient.gift} </p>
+    <p class ="budget">Budget of: $${recipient.budget}</p>
+    </div>
+    <div class="button">
+    <button class = "bought" id=${recipient.id}>Bought!</button>
+    </div>
+    `
+    document.querySelector('#recipient-list').appendChild(card)
+  
+    
+    const button = document.getElementById(`${recipient.id}`)
+    button.addEventListener("click", () => {
+      alert("Great Job! Who's Next?")
+      card.remove()
+      // removeRecipient(recipient.id) cause i keep testing and deleting it lol
+    })
+    
+  } 
+  
+  function budgetSum(){
+    let sum = 0
+    const bud1 = parseInt(document.getElementsByClassName('budget')[0].innerText.slice(12))
+    const bud2 = parseInt(document.getElementsByClassName('budget')[2].innerText.slice(12))
+    const bud3 = parseInt(document.getElementsByClassName('budget')[3].innerText.slice(12))
+   console.log(parseInt(document.getElementsByClassName('budget')[0].slice(12)))
+  // for (let i=0; i <; i++)
+  } budgetSum()
+
+// fetches // 
+
+function getRecipients(){
+  fetch('http://localhost:3000/recipients')
+  .then(r => r.json())
+  .then(data => data.forEach(recipient => renderOneRecipient(recipient)) )
+}
+
+
+function addRecipient(event){
+    event.preventDefault()
+    const [name, gift, budget] = event.target
+  
+    fetch("http://localhost:3000/recipients",{
+      method: "POST",
+      headers:{
+      "Content-Type": "application/json",
+    },
+      body: JSON.stringify({
+      name: name.value,
+      gift: gift.value,
+      budget: budget.value
+      })
+    })
+    .then(response => response.json())
+    .then(response => renderOneRecipient(response))
+    name.value = ""
+    gift.value = ""
+    budget.value = ""
+  }
+
+
+
+function removeRecipient(id){
+  fetch(`http://localhost:3000/recipients/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+}
